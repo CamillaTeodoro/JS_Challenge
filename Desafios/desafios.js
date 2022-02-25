7fcea99370603044772a474003be3f21ffc8e175
Camilla <camillateodoro@hotmail.com>
2022-02-25 14:03:43 -0300

    25/02 - parte da manhã

--- Desafios/desafios.js
+++ Desafios/desafios.js
@@ -72,18 +72,16 @@
 
 //! Leia um valor inteiro X. Em seguida apresente os 6 valores ímpares consecutivos a partir de X, um valor por linha, inclusive o X se for o caso.
 
-let x = 111;
+// let x = 110;
 
-if (x % 2 === 0) {
-  x += 1;
-} else {
-  x;
-}
+// if (x % 2 === 0) {
+//   x += 1;
+// }
 
-for (let i = 0; i < 6; i++) {
-  console.log(x);
-  x += 2;
-}
+// for (let i = 0; i < 6; i++) {
+//   console.log(x);
+//   x += 2;
+// }
 
 //! Recebe dois numeros, se a for menor que b divide a por b e se a for maior que b multiplica a com b.
 
@@ -122,3 +120,90 @@ for (let i = 0; i < 6; i++) {
 
 // console.log(myFunction([1, 3, 2]));
 // console.log(myFunction([4, 2, 3, 1]));
+
+//! Write a function that takes as argument a date instance (a) and a number (b). It should add b days to a and return the number of milliseconds since January 1, 1970, 00:00:00 UTC
+
+// function myFunction(a, b) {
+//   let days = b * 24 * 60 * 60 * 1000;
+
+//   let time = Date.parse(a);
+
+//   return time + days;
+// }
+
+// console.log(myFunction(new Date(Date.UTC(2000, 0o1, 0o1)), 31));
+
+// console.log(myFunction(new Date(Date.UTC(2000, 0o1, 0o1)), 10));
+
+// console.log(myFunction(new Date(Date.UTC(2000, 0o2, 28)), 2));
+
+//! Write a function that takes an object as argument. It should return an object with all original object properties except for the property with key 'b'
+
+// function myFunction(obj) {
+//   const myObj = delete obj.b;
+
+//   return obj;
+// }
+
+// console.log(myFunction({ a: 1, b: 7, c: 3 }));
+// console.log(myFunction({ b: 0, a: 7, d: 8 }));
+// console.log(myFunction({ e: 6, f: 4, b: 5, a: 3 }));
+
+//! Write a function that takes three elements of any type as arguments. Create a Set from those elements. Return the result
+
+// function myFunction(a, b, c) {
+//   const mySet = new Set([a, b, c]);
+//   return mySet;
+// }
+
+// console.log(myFunction(1, "b", 3));
+// console.log(myFunction(NaN, null, false));
+// console.log(myFunction("a", ["b"], { c: 3 }));
+
+//! Recebe um set e um valor e retorna o set com o valor deletado
+
+// function myFunction(set, val) {
+//   set.delete(val);
+//   return set;
+// }
+
+// console.log(myFunction(new Set([1, 2, 3]), 1));
+// // Expected new Set([2, 3])
+// console.log(myFunction(new Set("12345"), "3"));
+// // Expected new Set(['1', '2', '4', '5'])
+// console.log(myFunction(new Set([1, 2, 3]), 4));
+// // Expected new Set([1, 2, 3])
+
+//! Write a function that takes an object with two properties as argument. It should return the value of the property with key 'prop-2'
+
+// function myFunction(obj) {
+//   const myObj = obj["prop-2"];
+//   return myObj;
+// }
+
+// console.log(myFunction({ one: 1, "prop-2": 2 }));
+// console.log(myFunction({ "prop-2": "two", prop: "test" }));
+
+
+//! Write a function that takes two date instances as arguments. It should return true if the dates are equal or false otherwise.
+
+function myFunction(a, b) {
+
+    return a.getTime() === b.getTime()
+
+    // const dateOne = Date.parse(a);
+    // const dateTwo = Date.parse(b);
+
+    // if (dateOne === dateTwo) {
+    //     return true
+    // } else {
+    // return false;
+    // }
+  }
+  
+  console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:45:00')));
+  console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:00:00')));
+  console.log(myFunction(new Date('2001/01/01 08:00:00'), new Date('2000/01/01 08:00:00')));
+
+
+  
